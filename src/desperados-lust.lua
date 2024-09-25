56c817b1c405f5f98e53b5c739db02f1fa737d3a
Mikael Grimstad <mikael.grimstad@gmail.com>
2024-09-25 22:48:58 +0200

    fixed bug where aura registers on all units, not only self

--- src/desperados-lust.lua
+++ src/desperados-lust.lua
@@ -3,6 +3,9 @@ print("Flåklypa desperados lust loaded!")
 local lustNames = {"Heroism", "Bloodlust", "Fury of the Aspects", "Time Warp", "Primal Rage", "Feral Hide Drums", "Thunderous Drums"}
 
 local function OnEvent(self, event, unit, info)
+	if unit ~= "player" then
+        return
+    end
 	if info.addedAuras then
 		for _, v in pairs(info.addedAuras) do
 			for _, value in ipairs(lustNames) do
