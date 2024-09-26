25cec020db7c7a66354e10e87875f14625a86555
Mikael Grimstad <mikael.grimstad@gmail.com>
2024-09-26 23:00:06 +0200

    added grace time to trigger to avoid Fury of the aspects bug

--- src/desperados-lust.lua
+++ src/desperados-lust.lua
@@ -1,7 +1,7 @@
 print("Flåklypa desperados lust loaded!")
 
 local lustNames = {"Heroism", "Bloodlust", "Fury of the Aspects", "Time Warp", "Primal Rage", "Feral Hide Drums", "Thunderous Drums"}
-
+local lastTrigger;
 local function OnEvent(self, event, unit, info)
 	if unit ~= "player" then
         return
@@ -9,8 +9,11 @@ local function OnEvent(self, event, unit, info)
 	if info.addedAuras then
 		for _, v in pairs(info.addedAuras) do
 			for _, value in ipairs(lustNames) do
-				if value == v.name and v.duration ~= nil and v.duration > 35 then
-					PlaySoundFile("Interface\\AddOns\\Desperados-Lust\\flaklypa.mp3", "Master");
+				if value == v.name and v.duration ~= nil then
+					if lastTrigger == nil or (GetTime() - lastTrigger) > 45 then
+						lastTrigger = GetTime();
+						PlaySoundFile("Interface\\AddOns\\Desperados-Lust\\flaklypa.mp3", "Master");
+					end
 				end
 			end
 		end
